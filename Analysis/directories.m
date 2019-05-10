fa7e1f364e91fafdc9565bdcccd419842233cf13
johnleeuk12 <johnleeuk12@gmail.com>
2019-05-10 11:41:53 -0400

    GPU coding

--- Analysis/directories.m
+++ Analysis/directories.m
@@ -4,14 +4,16 @@ function fpath = directories(PC_name,animal_name,session_name)
 if strcmp(PC_name,'426_John')
     start_path = 'C:\Users\John.Lee\';
     kilo_path = 'copy\KiloSort';
+    analysis_path  ='\analysis-tools';
 elseif strcmp(PC_name,'Chamber_T') || strcmp(PC_name,'426_Analysis')
     start_path = 'C:\Users\Seth\';
     kilo_path = 'KiloSort';
+    analysis_path = '\analysis-tools-master';
 end
 
 addpath(genpath(fullfile(start_path, 'Documents\GitHub\', kilo_path))); % path to kilosort folder
 addpath(genpath(fullfile(start_path, 'Documents\GitHub\npy-matlab'))); % path to npy-matlab scripts
-addpath(genpath(fullfile(start_path,'Documents\GitHub\analysis-tools')));
+addpath(genpath(fullfile(start_path,'Documents\GitHub', analysis_path)));
 
 addpath(fullfile('C:\DATA\OpenEphys\', animal_name));
 fpath = fullfile('C:\DATA\OpenEphys\', animal_name, filesep, session_name);% where on disk do you want the simulation? ideally and SSD...
