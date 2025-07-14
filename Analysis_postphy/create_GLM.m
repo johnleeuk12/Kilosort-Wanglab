218e02176eec274dacd833ce34584a8c297aa93b
Jong Hoon Lee <35505724+johnleeuk12@users.noreply.github.com>
2025-07-14 14:37:14 +0900

    cleanup commit

--- Analysis_postphy/create_GLM.m
+++ Analysis_postphy/create_GLM.m
@@ -1,5 +1,5 @@
 % add to pool 
-function create_GLM(Pool,name)
+function create_GLM(Pool,name,D)
 
 
 %{
@@ -9,6 +9,11 @@ UPDATE LOG
 20/01/2025 JHL
 Adding transition phase to hit rate.
 adding trialtype id to data
+
+14/07/2025 JHL
+
+before running the code, load D as the following: 
+D = load('D:\DATA\List data\ICPool_Event_TTR.mat');
 
 
 %}
@@ -24,36 +29,36 @@ for p = 1:length(Pool)
 
 end
 
+%% Adding TTR and Lickbout onset times
+
+dataset= D.dataset;
+
+for n = 1:length(GLM_dataset)
+    lick_bouts = dataset{n,2}((dataset{n,2}(:,3)>0),1)*1e-3;
+    for tr = 1:length(GLM_dataset{n,3})
+        if tr == length(GLM_dataset{n,3})
+            diff = lick_bouts(find(lick_bouts>GLM_dataset{n,3}(tr,1)));
+        else
+            diff = lick_bouts(find(lick_bouts>GLM_dataset{n,3}(tr,1) & lick_bouts<GLM_dataset{n,3}(tr+1,1)));
+        end
+        if length(diff)>1
+            diff = diff(1);
+        end
+        if isempty(diff)
+            GLM_dataset{n,3}(tr,2) = 0;
+        else
+            GLM_dataset{n,3}(tr,2) = diff-GLM_dataset{n,3}(tr,1);
+        end
+        if  GLM_dataset{n,3}(tr,2) > 8
+            GLM_dataset{n,3}(tr,2) = 0;
+        end
+        
+    end
+    
+    GLM_dataset{n,7} = dataset{n,5}+200;
+end
+
+%%
 
 save(fullfile('D:\DATA',filesep,[name,'.mat']),"GLM_dataset")
 
-
-
-
-
-
-
-
-% 
-% sz = length(GLM_dataset);
-% for p = 1:length(Pool)
-%     GLM_dataset{p+sz,1} = Pool(p).spiketimes;
-%     GLM_dataset{p+sz,2} = Pool(p).licktimes;
-%     GLM_dataset{p+sz,4} = Pool(p).eventtimes;
-%     GLM_dataset{p+sz,5} = Pool(p).best_ch;
-%     idx = find(Pool(p).xb.trial_type(:,4) < 5);
-%     M = zeros(length(idx),2);
-%     for i = 1:length(idx)
-%         if mod(Pool(p).xb.trial_type(i,4),2) ==1
-%             M(i,1) = 1;
-%             M(i,2) = 1;
-%         elseif  mod(Pool(p).xb.trial_type(i,4),2) ==3
-%             M(i,1) = 1;
-%         elseif  mod(Pool(p).xb.trial_type(i,4),2) ==4
-%             M(i,2) = 1;
-%         end
-%     end
-%     
-%         
-%     GLM_dataset{p+sz,3} = [M(1:length(Pool(p).xb.hit_code),:),Pool(p).xb.hit_code];
-% end
